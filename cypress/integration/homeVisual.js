962b0dd665481d8dcf4c2902db5325c9a5055b0e
bogdan doroshenko <bogdan@onix-systems.com>
2020-06-16 21:13:47 +0300

    update

---
+++ cypress/integration/homeVisual.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe('Visual Test for HomePage', function() 
+{
+
+it('Open Home Page',function() {
+    //cy.percySnapshot('Home')
+    cy.visit('https://onix-systems.com/')
+    //cy.percySnapshot('HomeDowloaded')
+  
+})
+})
