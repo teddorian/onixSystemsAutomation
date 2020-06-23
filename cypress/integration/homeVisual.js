af0c265c312bd0c1ba6a7f83315f4732dae4ba21
bogdan doroshenko <bogdan@onix-systems.com>
2020-06-23 17:49:00 +0300

    changes in cypress.json pakage.json

--- cypress/integration/homeVisual.js
+++ cypress/integration/homeVisual.js
@@ -4,9 +4,9 @@ describe('Visual Test for HomePage', function()
 {
 
 it('Open Home Page',function() {
-    cy.percySnapshot('Home')
+    
     cy.visit('https://onix-systems.com/')
-    //cy.percySnapshot('HomeDowloaded')
+    cy.percySnapshot('HomeDowloaded')
   
 })
 })
